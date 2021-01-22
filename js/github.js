58b4afdf141c8901afff26fd33fde922be12fd5b
mgeissen <matt.geis97@gmail.com>
2021-01-22 18:47:11 +0100

    fix request url

--- js/github.js
+++ js/github.js
@@ -3,14 +3,12 @@ const repo = 'bookmarks';
 const branch = 'master';
 
 function onSubmit(form) {
-    // 1
     const login = form.username || form.querySelector('#login').value;
     const password = form.token || form.querySelector('#password').value;
 
-    // 2
     const token = btoa(`${login}:${password}`);
     const request = new Request(
-        `https://api.github.com/repos/${org}/${repo}/contents/${page}?ref=${branch}`,
+        `https://api.github.com/repos/${org}/${repo}/contents/overview?ref=${branch}`,
         {
             method: 'GET',
             credentials: 'omit',
@@ -20,7 +18,6 @@ function onSubmit(form) {
             },
         });
 
-    // 3
     fetch(request)
         .then(function (response) {
             if (response.status !== 200) { // 4
